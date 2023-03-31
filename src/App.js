7e4b1bde8d41333a9c97002d98a2c939ed013abe
stelda <stelda@outlook.fr>
2023-03-31 10:17:46 +0200

    Reorganizes code for improved clarity

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import {useEffect, useState, useRef} from "react";
-import RAPID_API_KEY from "./.env";
 import SingleCard from "./components/SingleCard";
 import './App.css';
 
@@ -22,39 +21,19 @@ function App() {
         {   "src": "/img/sud.png", matched: false },
         {   "src": "/img/ouest.png", matched: false },
     ]
-
     
-
     const [cards, setCards] = useState([])
     const [turns, setTurns] = useState(0)
     const [choiceOne, setChoiceOne] = useState(null)
     const [choiceTwo, setChoiceTwo] = useState(null)
     const [disabled, setDisabledUI] = useState(false)
+    
     const audioRef = useRef(null);
-    const [fetchedCharacter, setFetchedCharacter] = useState("")
 
-    // API fetch
-    const options = {
-        method: 'GET',
-        headers: {
-            'X-RapidAPI-Key': RAPID_API_KEY,
-            'X-RapidAPI-Host': 'kanjialive-api.p.rapidapi.com'
-        }
-    };
-
-    useEffect(() => {
-        const fetchData = async () => {
-            const response = await fetch('https://kanjialive-api.p.rapidapi.com/api/public/search/advanced/?on=%E3%82%B7%E3%83%B3', options);
-            const data = await response.json();
-            setFetchedCharacter(data[0].kanji.character);
-        };
-        fetchData();
-    }, []);
-
-
+    
 // =================================== behaviors ===================================
 
-    /* définit la fonction shuffledCards qui retourne un tableau de cartes mélangées */
+    // Shuffles cards
     const shuffleCards = () => {
         const shuffledCards = [...Cards, ...Cards]
             .sort(() => Math.random() - 0.5)
@@ -66,11 +45,19 @@ function App() {
         setTurns(0)
     }
 
-    /* définit la fonction handleChoice */
+    // Handles card selection
     const handleChoice = (card) => {
         choiceOne ? setChoiceTwo(card) : setChoiceOne(card)
     }
 
+    // Reset choices and increments turns
+    const resetTurn = () => {
+        setChoiceOne(null)
+        setChoiceTwo(null)
+        setTurns(prevTurns => prevTurns +1)
+        setDisabledUI(false)
+    }
+    
     // Compares two selected cards
     useEffect(()=>{
         if (choiceOne && choiceTwo) {
@@ -94,14 +81,6 @@ function App() {
         }
     }, [choiceOne, choiceTwo])
 
-    // Reset choices and increments turns
-    const resetTurn = () => {
-        setChoiceOne(null)
-        setChoiceTwo(null)
-        setTurns(prevTurns => prevTurns +1)
-        setDisabledUI(false)
-    }
-
     // Starts a game automatically
     useEffect(() => {
         shuffleCards()
@@ -109,8 +88,8 @@ function App() {
 
 
 // =================================== render ===================================
-  return (
-      <div className="App">
+return (
+    <div className="App">
         <h1>kanjimori</h1>
         <button onClick={shuffleCards}>New Game</button>
         <div className="card-grid">
@@ -121,17 +100,15 @@ function App() {
                     handleChoice={handleChoice}
                     flipped={card === choiceOne || card === choiceTwo || card.matched}
                     disabled={disabled}
-                    audioRef={audioRef}
-                    character={fetchedCharacter}
-
+                    audioRef={audioRef}                    
                 />
             ))}
         </div>
         <div>
             <p>Turns: {turns}</p>
         </div>
-     </div>
-  );
+    </div>
+);
 }
 
 export default App;
--- src/components/SingleCard.js
+++ src/components/SingleCard.js
@@ -1,5 +1,5 @@
 import "./SingleCard.css"
-export default function SingleCard({card, handleChoice, flipped, disabled, audioRef, character}){
+export default function SingleCard({card, handleChoice, flipped, disabled, audioRef}){
 
     const handleClick = () => {
         if (!disabled){
@@ -21,7 +21,7 @@ export default function SingleCard({card, handleChoice, flipped, disabled, audio
                     <source src="/sounds/drum.mp3" type="audio/mpeg" />
                     <source src="/sounds/drum.wav" type="audio/wav" />
                 </audio>
-                {/*<p>{character}</p>*/}
+
             </div>
         </div>
     );
